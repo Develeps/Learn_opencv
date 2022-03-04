d4d9ee9b2aa1ecfa3da8dd96f327538a39e90541
Sugu <42416961+Develeps@users.noreply.github.com>
2022-03-04 13:02:41 +0700

    sada

--- #1 OpenCV - работа с изображениями/main.py
+++ #1 OpenCV - работа с изображениями/main.py
@@ -3,16 +3,19 @@ import numpy as np
 
 img = cv2.imread("image/stop.jpg")
 
+#-------------------------------------------------------------
 re_img = cv2.resize(img, (img.shape[1]//2, img.shape[0]//2))
 #cv2.imshow("result",re_img[0:400, 0:450])
 
+#-------------------------------------------------------------
 #blur_img = cv2.GaussianBlur(re_img, (201,201), 0)
 #cv2.imshow("blur", blur_img)
 
+#-------------------------------------------------------------
 #img_col = cv2.cvtColor(re_img, cv2.COLOR_BGR2GRAY)
 #cv2.imshow("silver", img_col)
 
-
+#-------------------------------------------------------------
 arr = np.ones((2, 2), np.uint8)
 img_v = cv2.Canny(re_img, 200, 200)
 img_v = cv2.dilate(img_v, arr, iterations=1)
